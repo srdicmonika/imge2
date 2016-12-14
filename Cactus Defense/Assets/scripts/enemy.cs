580c9e0d2ed7af135a5ef2b1b07857ba1ff87222
Monika Srdic <srdic@in.tum.de>
2016-12-14 18:03:43 +0100

    renamed enemy scripts

--- Cactus Defense/Assets/scripts/enemy.cs
+++ Cactus Defense/Assets/scripts/enemy.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class player : MonoBehaviour {
+public class enemy : MonoBehaviour {
 
 	public int speed;
 	// Use this for initialization
