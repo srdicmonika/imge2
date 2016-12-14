9caf3b6c986accdf3572af453dcf82eb81e87bb2
Jonathan <jjjooonnn@gmail.com>
2016-12-14 17:36:38 +0100

    Mistake lololol

---
+++ Cactus Defense/Assets/scripts/ScoreManager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreManager : MonoBehaviour {
+    public int scr;
+    public int enrg;
+    void OnGUI()
+    {
+
+        GUI.Box(new Rect(30, Screen.height/3- 10, 100, 30), "Score: " + scr);
+
+        GUI.Box(new Rect(30, Screen.height * 2 / 3 - 10, 100, 30),"Energy:" + enrg);
+
+    }
+}
