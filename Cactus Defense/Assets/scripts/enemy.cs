4189defc5348c17ccf4d5c3be74b9860b513a90b
Monika Srdic <srdic@in.tum.de>
2016-12-20 11:21:48 +0100

    enemy has just one script now

--- Cactus Defense/Assets/scripts/enemy.cs
+++ Cactus Defense/Assets/scripts/enemy.cs
@@ -4,7 +4,15 @@ using UnityEngine;
 
 public class enemy : MonoBehaviour {
 
+	public GameObject enemiezPrefab;
+	GameObject enemiezClones;
+	public float timeLeft;
+	//sets random spawnpoint
 	public int speed;
+	int randNr;
+	//sets target of enemies, where they want to go!
+	GameObject target;
+
 	// Use this for initialization
 	void Start () {
 		
@@ -13,6 +21,19 @@ public class enemy : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		//player moves towards tower 
+		timeLeft -= Time.deltaTime;
+		if ( timeLeft < 0 )
+		{
+			timeLeft = 4;
+			randNr = Random.Range (1, 3);
+			Debug.Log (randNr);
+			if (randNr == 1) {
+				enemiezClones = Instantiate (enemiezPrefab, GameObject.Find("spawnpoint1").transform.position, Quaternion.identity) as GameObject;
+			} 
+			if (randNr == 2) {
+				enemiezClones = Instantiate (enemiezPrefab,GameObject.Find("spawnpoint2").transform.position, Quaternion.identity) as GameObject;
+			}
+		}
 		transform.Translate(Vector3.left* speed * Time.deltaTime);
 	}
 	void OnTriggerEnter(Collider other) {
@@ -21,3 +42,4 @@ public class enemy : MonoBehaviour {
 		}
 	}
 }
+
